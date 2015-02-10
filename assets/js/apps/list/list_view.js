7ecad7575ea4c12425da20f064e0c674b0fe685e
paul.chang <paul.chang@climate.com>
2015-02-09 20:27:13 -0800

    deleting the row from the collection

--- assets/js/apps/list/list_view.js
+++ assets/js/apps/list/list_view.js
@@ -6,12 +6,25 @@ ContactManager.module('ContactsApp.List',
         template: '#contact-list-item',
 
         events: {
-          click: 'highlightName'
+          click: 'highlightName',
+          'click button.js-delete': 'deleteClicked'
         },
 
         highlightName: function (e) {
           e.preventDefault();
           this.$el.toggleClass('warning');
+        },
+
+        deleteClicked: function (e) {
+          e.stopPropagation();
+          /*
+            Although we don’t have direct access to the contacts collection,
+            each model keeps a reference to its parent collection within the
+            collection attribute. Therefore, we can access our contacts
+            collection from within our Contact view instance with
+            this.model.collection.
+           */
+          this.model.collection.remove(this.model);
         }
       });
 

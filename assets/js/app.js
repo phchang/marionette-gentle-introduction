c21c461cc540efe23feb56b98ec77944a9bfffb7
paul.chang <paul.chang@climate.com>
2015-02-10 07:46:01 -0800

    making helper methods for Backbone.history

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,20 +4,27 @@ ContactManager.addRegions({
   mainRegion: '#main-region'
 });
 
+ContactManager.navigate = function (route, options) {
+  options || (options = {});
+
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
 ContactManager.on('start', function() {
+
+  var ContactsAppList = ContactManager.module('ContactsApp.List');
 
   if (Backbone.history) {
     Backbone.history.start();
 
-    if (Backbone.history.fragment === '') {
-      Backbone.history.navigate('contacts');
-      ContactManager.ContactsApp.List.Controller.listContacts();
+    if (this.getCurrentRoute() === '') {
+      this.navigate('contacts');
+      ContactsApp.List.Controller.listContacts();
     }
   }
-
-  var ContactsAppList = ContactManager.module('ContactsApp.List');
-
-  ContactsAppList.Controller.listContacts();
-
 });
 

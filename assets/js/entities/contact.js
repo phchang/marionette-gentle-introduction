4f9df45d20954da7bef401fe0e25dba1b2cff71e
paul.chang <paul.chang@climate.com>
2015-02-09 16:57:38 -0800

    introducing modules, breaking out the app declaration to a separate file, implementing Marionette.RequestResponse handling

---
+++ assets/js/entities/contact.js
@@ -0,0 +1,55 @@
+/* http://marionettejs.com/docs/marionette.module.html */
+
+ContactManager.module('Entities',
+    function (Entities, ContactManager, Backbone, Marionette, $, _) {
+
+      Entities.Contact = Backbone.Model.extend({});
+
+      Entities.ContactCollection = Backbone.Collection.extend({
+
+        model: Entities.Contact,
+
+        comparator: function (model) {
+          return model.get('firstName') + model.get('lastName');
+        }
+      });
+
+      var contacts;
+
+      var initializeContacts = new Entities.ContactCollection([
+         contacts = new Entities.ContactCollection([
+           {
+             id: 1,
+             firstName: 'Alice',
+             lastName: 'Arten',
+             phoneNumber: '555-0184'
+           },
+           {
+             id: 2,
+             firstName: 'Bob',
+             lastName: 'Brigham',
+             phoneNumber: '555-0163'
+           },
+           {
+             id: 3,
+             firstName: 'Charlie',
+             lastName: 'Campbell',
+             phoneNumber: '555-0129'
+           }
+         ])
+      ]);
+
+      var API = {
+        getContactEntities: function () {
+          if (contacts === undefined) {
+            initializeContacts();
+          }
+
+          return contacts;
+        }
+      };
+
+      ContactManager.reqres.setHandler('contact:entities', function () {
+        return API.getContactEntities();
+      });
+    });

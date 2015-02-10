7d536178578a0fef0ad8a565c3c0649c7f21dacb
paul.chang <paul.chang@climate.com>
2015-02-09 19:45:21 -0800

    refactoring remaining calls into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,12 @@ var ContactManager = new Marionette.Application();
 ContactManager.addRegions({
   mainRegion: '#main-region'
 });
+
+ContactManager.on('start', function() {
+
+  var ContactsAppList = ContactManager.module('ContactsApp.List');
+
+  ContactsAppList.Controller.listContacts();
+
+});
+
